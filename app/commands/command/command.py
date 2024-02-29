6acf78d4fd3cf3dfa1c65e9d602e91be5fa5cafb
russf19 <russ_f19@verizon.net>
2024-02-28 21:13:45 -0500

    added commands

---
+++ app/commands/command/command.py
@@ -0,0 +1,6 @@
+# Master command class thats sets the structure of all commands
+
+class Command:
+    def execute(self):
+        raise NotImplementedError("Commands MUST implement an execute method.")
+    
