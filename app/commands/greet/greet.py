5c75abc4e18e79cff6a69fb4277845211eb48d21
russf19 <russ_f19@verizon.net>
2024-03-06 17:35:51 -0500

    added logging.conf and modified greet.py and requirements

--- app/commands/greet/greet.py
+++ app/commands/greet/greet.py
@@ -1,6 +1,16 @@
 from app.commands.command.command import Command
+import logging
+
+class test():
+    myval = 10
+    yourval = 11
 
 class GreetCommand(Command):
     def execute(self):
-        return "Hello, world!"
+        logging.info("Hello World!")
+
+        mydict = {1,2,3,4,5}
+        logging.debug(print(test))
+
+        print("Hello, World!")
     
